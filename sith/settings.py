42d776cfcd6196122be827641a72e3d5c035644e
Sayn0toWar <yestonature@gmail.com>
2019-11-30 17:38:00 +0400

    Deploy

--- sith/settings.py
+++ sith/settings.py
@@ -124,6 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].upgrade(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].upgrade(db_from_env)
