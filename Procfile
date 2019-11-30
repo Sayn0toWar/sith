68d830323ac7ffa014aa4eaac3cc16e9842859d8
Sayn0toWar <yestonature@gmail.com>
2019-11-30 17:14:01 +0400

    Procfile, runtime and requirements added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn sithtest.wsgi --log-file -
--- sith/settings.py
+++ sith/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r8@48n^(r&$x2x*%w26hqeygs@-f(e#*9$-@4*rh-r2&@(_-w)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].upgrade(db_from_env)
