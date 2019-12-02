46ded81be81e641dcd374ec81a565ac1865c050c
Sayn0toWar <yestonature@gmail.com>
2019-12-02 22:56:18 +0400

    Deploy

--- sith/settings.py
+++ sith/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +130,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
